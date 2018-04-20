7f0a1db47cdcc0189675b2af0bf4f3bcfdb80c7e
cristianraiber <cristian@machothemes.com>
2018-04-20 17:36:08 +0300

    simplified script

--- deploy.sh
+++ deploy.sh
@@ -2,152 +2,86 @@
 
 set -e
 
-#
-# Script should never be ran outside of TravisCI
-#
-if [[ -z "$TRAVIS" ]]; then
-	echo "Script is only to be run by Travis CI" 1>&2
-	exit 1
-fi
-
-#
-# Check if WP_ORG username/passwords are set in travis.yml
-#
-if [[ -z "$WP_ORG_PASSWORD" || "$WP_ORG_USERNAME" ]]; then
-	echo "WordPress.org credentials aren't set" 1>&2
-	exit 1
-fi
-
-#
-# Check if this is being ran on master branch
-#
-if [[ -z "$TRAVIS_BRANCH" || "$TRAVIS_BRANCH" != "master" ]]; then
-	echo "Build branch is required and must be 'master'" 1>&2
-	exit 0
-fi
-
-#
-# Make sure this isn't a pull request; we don't build & release on pull-requests
-#
 if [[ "false" != "$TRAVIS_PULL_REQUEST" ]]; then
 	echo "Not deploying pull requests."
 	exit
 fi
 
-
-PLUGIN=`echo basename "$PWD"`                                                                                   # get repo slug and use it as the plugin slug when committing to SVN; L71
-PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
-PLUGIN_BUILDS_PATH="$PROJECT_ROOT/builds"
-PLUGIN_BUILD_CONFIG_PATH="$PROJECT_ROOT/build-cfg"
-VERSION=$(/usr/bin/php -f "$PLUGIN_BUILD_CONFIG_PATH/utils/get_plugin_version.php" "$PROJECT_ROOT" "$PLUGIN")
-ZIP_FILE="$PLUGIN_BUILDS_PATH/$PLUGIN-$VERSION.zip"
-
-#
-# Ensure the zip file for the current version has been built
-#
-if [ ! -f "$ZIP_FILE" ]; then
-    echo "Built zip file $ZIP_FILE does not exist" 1>&2
-    exit 1
+if [[ ! $SVN_REPO ]]; then
+	echo "SVN repo is not specified."
+	exit
 fi
 
-#
-# Check if the tag exists for the version we are building
-#
-TAG=$(svn ls "https://plugins.svn.wordpress.org/$PLUGIN/tags/$VERSION")
-error=$?
-if [ $error == 0 ]; then
-    # Tag exists, don't deploy
-    echo "Tag already exists for version $VERSION, aborting deployment"
-    exit 1
+if [[ -z "$TRAVIS_BRANCH" || "$TRAVIS_BRANCH" != "master" ]]; then
+	echo "Build branch is required and must be 'master'" 1>&2
+	exit
 fi
 
-#
-# Remove any unzipped dir so we start from scratch
-#
-cd "$PLUGIN_BUILDS_PATH"
-rm -fR "$PLUGIN"
+if [[ -z "$TRAVIS_TAG" ]]; then
+	echo "This isn't a tagged release" 1>&2
+	exit
+fi
 
-# Unzip the built plugin
-unzip -q -o "$ZIP_FILE"
+# Untrailing slash of SVN_REPO path
+SVN_REPO=`echo $SVN_REPO | sed -e "s/\/$//"`
+# Git repository
+GH_REF=https://github.com/${TRAVIS_REPO_SLUG}.git
 
-# Clean up any previous svn dir
-rm -fR svn
+echo "Starting deploy..."
 
-# Checkout the SVN repo
-svn co -q "http://svn.wp-plugins.org/$PLUGIN" svn
+mkdir build
 
-#
-# Move out the trunk directory to a temp location
-#
-mv svn/trunk ./svn-trunk
+cd build
+BASE_DIR=$(pwd)
 
-#
-# Create trunk directory
-#
-mkdir svn/trunk
+echo "Checking out trunk from $SVN_REPO ..."
+svn co -q $SVN_REPO/trunk
 
-#
-# Copy our new version of the plugin into trunk
-#
-rsync -r -p $PLUGIN/* svn/trunk
+echo "Getting clone from $GH_REF to $SVN_REPO ..."
+git clone -q $GH_REF ./git
 
-#
-# Copy all the .svn folders from the checked out copy of trunk to the new trunk.
-# This is necessary as the Travis container runs Subversion 1.6 which has .svn dirs in every sub dir
-#
-cd svn/trunk/
-TARGET=$(pwd)
-cd ../../svn-trunk/
+cd ./git
 
-#
-# Find all .svn dirs in sub dirs
-#
-SVN_DIRS=`find . -type d -iname .svn`
+if [ -e "bin/build.sh" ]; then
+	echo "Starting bin/build.sh."
+	bash bin/build.sh
+fi
+
+cd $BASE_DIR
+
+echo "Syncing git repository to svn"
+rsync -a --exclude=".svn" --checksum --delete ./git/ ./trunk/
+rm -fr ./git
+
+cd ./trunk
 
 
-for SVN_DIR in $SVN_DIRS; do
-    SOURCE_DIR=${SVN_DIR/.}
-    TARGET_DIR=$TARGET${SOURCE_DIR/.svn}
-    TARGET_SVN_DIR=$TARGET${SVN_DIR/.}
-    if [ -d "$TARGET_DIR" ]; then
-        # Copy the .svn directory to trunk dir
-        cp -r $SVN_DIR $TARGET_SVN_DIR
-    fi
-done
+if [ -e ".svnignore" ]; then
+    echo "svn propset"
+    svn propset -q -R svn:ignore -F .svnignore .
+fi
 
-#
-# Back to builds dir
-#
-cd ../
 
-#
-# Remove checked out dir
-#
-rm -fR svn-trunk
+echo "Run svn add"
+svn st | grep '^!' | sed -e 's/\![ ]*/svn del -q /g' | sh
+echo "Run svn del"
+svn st | grep '^?' | sed -e 's/\?[ ]*/svn add -q /g' | sh
 
-#
-# Add new version tag
-#
-mkdir svn/tags/$VERSION
-rsync -r -p $PLUGIN/* svn/tags/$VERSION
+# If tag number and credentials are provided, commit to trunk.
+if [[ $TRAVIS_TAG && $WP_ORG_USERNAME && $WP_ORG_PASSWORD ]]; then
+	if [[ ! -d tags/$TRAVIS_TAG ]]; then
 
-#
-# Add new files to SVN
-#
-svn stat svn | grep '^?' | awk '{print $2}' | xargs -I x svn add x@
-
-#
-# Remove deleted files from SVN
-#
-svn stat svn | grep '^!' | awk '{print $2}' | xargs -I x svn rm --force x@
-svn stat svn
-
-#
-# Commit to SVN
-#
-svn ci --no-auth-cache --username $WP_ORG_USERNAME --password $WP_ORG_PASSWORD svn -m "Deploy version $VERSION"
-
-#
-# Remove SVN temp dir
-#
-rm -fR svn
+		# Start committing to SVN
+		echo "Commit to $SVN_REPO."
+		svn ci --no-auth-cache  commit -m "commit version $TRAVIS_TAG" --username $WP_ORG_USERNAME --password $WP_ORG_PASSWORD --non-interactive 2>/dev/null
+		
+		# Make a copy of `trunk` inside `tags` folder
+		echo "Take snapshot of $TRAVIS_TAG"
+		svn copy $SVN_REPO/trunk $SVN_REPO/tags/$TRAVIS_TAG -m "Take snapshot of $TRAVIS_TAG" --username $WP_ORG_USERNAME --password $WP_ORG_PASSWORD --non-interactive 2>/dev/null
+	else
+		echo "tags/$TRAVIS_TAG already exists."
+	fi
+else
+	echo "Nothing to commit and check \`svn st\`."
+	svn st
+fi
